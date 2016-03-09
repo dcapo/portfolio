414ddef850bd7ae44dded5ac1383ca16ac71ad33
Daniel Capo <danielccapo@gmail.com>
2016-03-09 01:29:36 -0800

    implement salvattore for portfolio items; move images to s3; .gitignore img/*

--- js/portfolio.js
+++ js/portfolio.js
@@ -15,47 +15,47 @@
 
     $(".bolt.thumbnail").on("click", openPhotoSwipe([
         {
-            src: '/img/bolt_0.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bolt_0.png',
             w: 1200,
             h: 858
         }, {
-            src: '/img/bolt_1.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bolt_1.png',
             w: 1200,
             h: 656
         }, {
-            src: '/img/bolt_2.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bolt_2.png',
             w: 1200,
             h: 858
         }, {
-            src: '/img/bolt_3.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bolt_3.png',
             w: 1200,
             h: 820
         }, {
-            src: '/img/bolt_4.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bolt_4.png',
             w: 1200,
             h: 820
         }, {
-            src: '/img/bolt_5.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bolt_5.png',
             w: 1200,
             h: 820
         }, {
-            src: '/img/bolt_6.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bolt_6.png',
             w: 1200,
             h: 820
         }, {
-            src: '/img/bolt_7.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bolt_7.png',
             w: 1200,
             h: 705
         }, {
-            src: '/img/bolt_8.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bolt_8.png',
             w: 1200,
             h: 820
         }, {
-            src: '/img/bolt_9.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bolt_9.png',
             w: 1200,
             h: 820
         }, {
-            src: '/img/bolt_10.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bolt_10.png',
             w: 1200,
             h: 820
         }
@@ -63,15 +63,15 @@
 
     $(".cornhole.thumbnail").on("click", openPhotoSwipe([
         {
-            src: '/img/cornhole_0.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/cornhole_0.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/cornhole_1.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/cornhole_1.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/cornhole_2.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/cornhole_2.jpg',
             w: 1200,
             h: 900
         }
@@ -79,31 +79,31 @@
 
     $(".set.thumbnail").on("click", openPhotoSwipe([
         {
-            src: '/img/set_0.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/set_0.png',
             w: 396,
             h: 744
         }, {
-            src: '/img/set_1.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/set_1.png',
             w: 640,
             h: 960
         }, {
-            src: '/img/set_2.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/set_2.png',
             w: 640,
             h: 960
         }, {
-            src: '/img/set_3.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/set_3.png',
             w: 396,
             h: 744
         }, {
-            src: '/img/set_4.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/set_4.png',
             w: 396,
             h: 744
         }, {
-            src: '/img/set_5.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/set_5.png',
             w: 396,
             h: 744
         }, {
-            src: '/img/set_6.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/set_6.png',
             w: 396,
             h: 744
         }
@@ -111,43 +111,43 @@
 
     $(".piano.thumbnail").on("click", openPhotoSwipe([
         {
-            src: '/img/piano_stand_0.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/piano_stand_0.png',
             w: 1434,
             h: 918
         }, {
-            src: '/img/piano_stand_1.png',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/piano_stand_1.png',
             w: 1434,
             h: 918
         }, {
-            src: '/img/piano_stand_2.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/piano_stand_2.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/piano_stand_3.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/piano_stand_3.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/piano_stand_4.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/piano_stand_4.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/piano_stand_5.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/piano_stand_5.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/piano_stand_6.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/piano_stand_6.jpg',
             w: 900,
             h: 1200
         }, {
-            src: '/img/piano_stand_7.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/piano_stand_7.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/piano_stand_8.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/piano_stand_8.jpg',
             w: 900,
             h: 1200
         }, {
-            src: '/img/piano_stand_9.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/piano_stand_9.jpg',
             w: 1200,
             h: 900
         }
@@ -155,91 +155,91 @@
 
     $(".bbq.thumbnail").on("click", openPhotoSwipe([
         {
-            src: '/img/bbq_0.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_0.jpg',
             w: 640,
             h: 480
         }, {
-            src: '/img/bbq_1.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_1.jpg',
             w: 640,
             h: 480
         }, {
-            src: '/img/bbq_2.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_2.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/bbq_3.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_3.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/bbq_4.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_4.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/bbq_5.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_5.jpg',
             w: 640,
             h: 480
         }, {
-            src: '/img/bbq_6.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_6.jpg',
             w: 640,
             h: 480
         }, {
-            src: '/img/bbq_7.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_7.jpg',
             w: 640,
             h: 480
         }, {
-            src: '/img/bbq_8.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_8.jpg',
             w: 640,
             h: 480
         }, {
-            src: '/img/bbq_9.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_9.jpg',
             w: 640,
             h: 480
         }, {
-            src: '/img/bbq_10.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_10.jpg',
             w: 640,
             h: 480
         }, {
-            src: '/img/bbq_11.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_11.jpg',
             w: 640,
             h: 480
         }, {
-            src: '/img/bbq_12.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_12.jpg',
             w: 640,
             h: 480
         }, {
-            src: '/img/bbq_13.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_13.jpg',
             w: 640,
             h: 480
         }, {
-            src: '/img/bbq_14.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_14.jpg',
             w: 640,
             h: 480
         }, {
-            src: '/img/bbq_15.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_15.jpg',
             w: 640,
             h: 480
         }, {
-            src: '/img/bbq_16.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_16.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/bbq_17.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_17.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/bbq_18.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_18.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/bbq_19.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_19.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/bbq_20.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_20.jpg',
             w: 1200,
             h: 900
         }, {
-            src: '/img/bbq_21.jpg',
+            src: 'https://s3-us-west-1.amazonaws.com/daniel-capo-portfolio/bbq_21.jpg',
             w: 1200,
             h: 900
         }
